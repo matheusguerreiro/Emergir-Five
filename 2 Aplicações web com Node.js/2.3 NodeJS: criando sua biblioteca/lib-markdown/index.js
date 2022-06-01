a810fe0163f577fef8be5cb933d608aa9c72c156
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-06-01 10:19:54 -0300

    aula 2 ok

--- 2 Aplicações web com Node.js/2.3 NodeJS: criando sua biblioteca/lib-markdown/index.js
+++ 2 Aplicações web com Node.js/2.3 NodeJS: criando sua biblioteca/lib-markdown/index.js
@@ -1,19 +1,48 @@
 /* 
  *  npm init -y
- *  npm install chalk
- *  .gitignore
  *  add "type": "module" inside package.json
- *  const fs = require('fs')
+ *  npm install chalk
+ *  import chalk from 'chalk'
+ *  .gitignore
+ *  import fs from 'fs'
+ *  erro
  */
 
 import chalk from 'chalk';
 import fs from 'fs'
 
-function pegaArquivo(caminhoDoArquivo) {
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
+}
+
+/* function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding, (_, conteudoDoArquivo) => {
-    console.log(chalk.yellow(conteudoDoArquivo))
+  fs.readFile(caminhoDoArquivo, encoding, (erro, conteudoDoArquivo) => {
+    if (erro) {
+      trataErro(erro)
+    }
+    console.log(chalk.green(conteudoDoArquivo))
   })
+} */
+
+// agora de forma assincrona com promises .then(() => ...) .catch(() => ...)
+/* function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8'
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((conteudoDoArquivo) => console.log(chalk.green(conteudoDoArquivo)))
+    .catch((erro) => trataErro(erro))
+} */
+
+// agora com async await
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8'
+  try {
+    const conteudoDoArquivo = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(conteudoDoArquivo))
+  } catch (erro) {
+    trataErro(erro)
+  }
 }
 
 pegaArquivo('./arquivos/texto1.md')
