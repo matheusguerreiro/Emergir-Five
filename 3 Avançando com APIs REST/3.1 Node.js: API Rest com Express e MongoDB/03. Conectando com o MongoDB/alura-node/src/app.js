38c8a25995e7037b8d4ad3c6a2eda9edaf320764
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-06-04 17:20:35 -0300

    c3 a3 - ok

---
+++ 3 Avançando com APIs REST/3.1 Node.js: API Rest com Express e MongoDB/03. Conectando com o MongoDB/alura-node/src/app.js
@@ -0,0 +1,51 @@
+import express from 'express'
+import db from './config/dbConnect.js'
+import livros from './models/Livro.js'
+
+db.on('error', console.log.bind(console, 'Erro de conexão!'))
+db.once('open', () => {
+  console.log('Conexão feita com sucesso')
+})
+
+const app = express()
+
+app.use(express.json())
+
+/* const livros = [
+  {id: 1, titulo: 'senhor dos aneis'},
+  {id: 2, titulo: 'o hobbit'}
+] */
+
+app.get('/', (req, res) => {
+  res.status(200).send('curso de node')
+})
+app.get('/livros', (req, res) => {
+  livros.find((err, livros) => {
+    res.status(200).json(livros)
+  })
+})
+app.get('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id)
+  res.json(livros[index])
+})
+app.post('/livros', (req, res) => {
+  livros.push(req.body)
+  res.status(201).send('livro cadastrado com sucesso')
+})
+app.put('/livros/:id', (req, res) => { 
+  let index = buscaLivro(req.params.id)
+  livros[index].titulo = req.body.titulo
+  res.json(livros)
+})
+app.delete('/livros/:id', (req, res) => {
+  let {id} = req.params
+  let index = buscaLivro(id)
+  livros.splice(index, 1)
+  res.send(`livro ${id} removido com sucesso`)
+})
+
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id)
+}
+
+export default app
